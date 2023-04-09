3225ced53d5c83227f8f4593de9de0b79b21c17a
daniko58 <80502676+daniko58@users.noreply.github.com>
2023-04-09 19:55:42 +1000

    Update script.js

--- script.js
+++ script.js
@@ -187,25 +187,9 @@ function printResult(e, text) {
   e.target.querySelector('.result').setHTML(text);
 }
 
-document.querySelector("#saveButton").addEventListener("click", changeMode);
 
-function changeMode() {
-    if (document.querySelector("#dark").checked == true) {
-        darkMode();
-    } else {
-        lightMode();
-    }
-}
 
-function darkMode() {
-    document.querySelector("body").style.backgroundColor = "black";
-    document.querySelector("body").style.color = "white";
-}
 
-function lightMode() {
-    document.querySelector("body").style.backgroundColor = "white";
-    document.querySelector("body").style.color = "black";
-}
 
 document.getElementById("pangram").addEventListener("submit", checkPangram);
 
@@ -218,4 +202,26 @@ document.getElementById('converter2').onsubmit = convertTextToMorseCode;
 function changeColour() {
     let colour = document.getElementById('colourInputText').value;
     document.body.style.backgroundColor =  colour;
+}
+
+(function() {
+  let onpageLoad = localStorage.getItem("theme") || "";
+  let element = document.body;
+  element.classList.add(onpageLoad);
+  document.getElementById("theme").textContent =
+    localStorage.getItem("theme") || "light";
+})();
+
+function themeToggle() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+
+  let theme = localStorage.getItem("theme");
+  if (theme && theme === "dark-mode") {
+    localStorage.setItem("theme", "");
+  } else {
+    localStorage.setItem("theme", "dark-mode");
+  }
+
+  document.getElementById("theme").textContent = localStorage.getItem("theme");
 }
